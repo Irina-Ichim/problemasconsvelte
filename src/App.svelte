c6760fdb09ab875bc0baa22986fd74dfdaaa8fa7
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-05-04 03:15:43 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  // Aquí se pueden agregar lógica y variables
+</script>
+
+<h1>Hola, mundo!</h1>
+<h2>El svelte no se quiere instalar en mi ordenador , asi que uso estos comandos siempre que deseo abrir un programa :
+	_npx degit sveltejs/template nombre del proyecto
+	_cd nombre del proyecto
+	_npm instal
+</h2>
+<h3>A continuacion npm run dev y miras el localhost</h3>
+<h4>Para crear tu propio proyecto vas a app.svelte eliminas todo y agregas lo que necesitas</h4>
+<style>
+  h1 {
+    font-size: 2em;
+    color: blue;
+  }
+  h2 {
+	font-size: 1em;
+	color: blue;
+  }
+  h3{
+	font-size: 1em;
+  }
+  h4{
+	font-size: 1em;
+	color:blue;
+  }
+</style>
+
+
